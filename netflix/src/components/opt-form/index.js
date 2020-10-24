b67e58fd4546750fd3206a5d772291ec2d45c5b8
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-24 14:33:49 -0300

    add opt-form

---
+++ netflix/src/components/opt-form/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Text, Input, Button } from './styles/opt-form.js';
+
+export default function OptForm({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+}
+
+OptForm.Input = function OptFormInput({ ...restProps }) {
+  return <Input {...restProps} />;
+};
+
+OptForm.Button = function OptFormButton({ children, ...restProps }) {
+  return (
+    <Button {...restProps}>
+      {children} <img src="/images/icons/chevron-right.png" alt="Try Now" />
+    </Button>
+  );
+};
+
+OptForm.Text = function OptFormText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
---
+++ netflix/src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  color: white;
+`;
+
+export const Input = styled.Input`
+  color: white;
+`;
+
+export const Button = styled.Button`
+  color: white;
+`;
+
+export const Text = styled.div`
+  color: white;
+`;
